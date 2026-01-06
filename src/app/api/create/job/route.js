ebc7bd3c3a0015a1e47c5b48c699ebe8dc9908d1
Karan Dalakoti <karandalakoti@Karans-MacBook-Air.local>
2026-01-06 22:21:38 +0530

    import path of ingestAiJobs changed in route create job

--- src/app/api/create/job/route.js
+++ src/app/api/create/job/route.js
@@ -1,5 +1,5 @@
 import { connectDB } from "@/lib/db";
-import { ingestAiJobs } from "@/services/jobIngestionService";
+import { ingestAiJobs } from "../../../../../services/jobIngestionService.js";
 import { NextResponse } from "next/server";
 
 export async function POST() {
